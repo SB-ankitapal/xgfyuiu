e142406b226bf6d096bf7f92fb7c6cada7f00425
ankita.pal@smartbear.com <ankita.pal@smartbear.com>
2025-08-01 11:47:48 +0530

    Added 15 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20209.stoplight-dev.com/api/v1/projects/cHJqOjI4ODIw/spectral.js?branch_slug=main&token=e684645c-1378-4656-ae62-079272235310";
+export default { extends: ruleset };
